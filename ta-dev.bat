d8c9022f6ac480c208216f49514994eb7a6eacdb
Elizabeth Sall <elizabeth@sfcta.org>
2014-07-09 11:50:56 -0700

    added binary installers to make it easy

--- ta-dev.bat
+++ ta-dev.bat
@@ -10,12 +10,10 @@ SET GT_DLD=https://github.com/msysgit/msysgit/releases/download/Git-1.9.4-previe
 SET GH_DLD=https://github-windows.s3.amazonaws.com/GitHubSetup.exe
 :-spyder v2.2.5 amd64
 SET SP_DLD=https://bitbucket.org/spyder-ide/spyderlib/downloads/spyder-2.2.5.win-amd64.exe
-:-scite
-set SC_DLD=http://prdownloads.sourceforge.net/scintilla/Sc344.exe
 :install
-for %i in (%GT_DLD% %GH_DLD% %SP_DLD% %SC_DLD%) DO (
-    echo %i
-    powershell -Command "Invoke-WebRequest %i -OutFile install.exe"
+for %%i in (%GT_DLD% %GH_DLD% %SP_DLD% %SC_DLD%) DO (
+    echo %%i
+    powershell -Command "Invoke-WebRequest %%i -OutFile install.exe"
     install.exe
 )
 :done
--- ta-python.bat
+++ ta-python.bat
@@ -1,7 +1,5 @@
 SET PY_DLD=https://www.python.org/ftp/python/2.7.7/python-2.7.7.amd64.msi
 SET PIP_DLD=https://bootstrap.pypa.io/get-pip.py
-SET NP_DLD=http://www.lfd.uci.edu/~gohlke/pythonlibs/5hgp4h3m/numpy-MKL-1.8.1.win-amd64-py2.7.exe
-SET PACKAGE_LIST=scipy,numexpr,networkx,pyro,simpleparse,tables,pandas,pywin32,pyshp,xlrd,xlwt
 
 :python
 powershell -Command "Invoke-WebRequest %PY_DLD% -OutFile python.msi"
@@ -11,10 +9,5 @@ SET PATH=%PATH%;C:\Python27;C:\Python27\Scripts
 :pypackages
 powershell -Command "Invoke-WebRequest %PIP_DLD% -OutFile get-pip.py"
 python get-pip.py
-powershell -Command "Invoke-WebRequest %NP_DLD% -OutFile numpy.exe"
-numpy.exe
-for %a in (%PACKAGE_LIST%) DO (
- echo %a
- pip install %a
-)
-:done
+pause
+:end
